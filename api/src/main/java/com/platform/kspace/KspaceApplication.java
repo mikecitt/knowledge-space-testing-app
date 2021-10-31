b01d8288d2e91638e6dc99b3f5e30cb1ffbbf24a
Mikec <mikecitt@gmail.com>
2021-10-31 19:14:01 +0100

    Initialization of UI and API projects. Added docs.

---
+++ api/src/main/java/com/platform/kspace/KspaceApplication.java
@@ -0,0 +1,13 @@
+package com.platform.kspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KspaceApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/platform/kspace/KspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform.kspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
