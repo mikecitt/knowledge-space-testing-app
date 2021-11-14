d06e3f8b198d454ec002b7cb1543ab8783a1a05a
rodusek-v <86666830+rodusek-v@users.noreply.github.com>
2021-11-14 13:30:52 +0100

    Small update

--- api/src/main/java/com/platform/kspace/service/UserService.java
+++ api/src/main/java/com/platform/kspace/service/UserService.java
@@ -3,8 +3,10 @@ package com.platform.kspace.service;
 import com.platform.kspace.dto.ProfessorDTO;
 import com.platform.kspace.dto.StudentDTO;
 import com.platform.kspace.exceptions.KSpaceException;
+import com.platform.kspace.model.User;
 
 public interface UserService {
     void registerStudent(StudentDTO registrationForm) throws KSpaceException;
     void registerProfessor(ProfessorDTO registrationForm) throws KSpaceException;
+    User findUserByEmail(String email) throws KSpaceException;
 }
