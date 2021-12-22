0ae0479b814d009ce195c1c924dae4889e02b5c4
Mikec <mikecitt@gmail.com>
2021-12-22 11:40:46 +0100

    Removed relation between Domain and Domain Problem on class diagram. Restored removed users for testing purposes.

--- api/src/main/java/com/platform/kspace/component/DataLoader.java
+++ api/src/main/java/com/platform/kspace/component/DataLoader.java
@@ -1,9 +1,39 @@
 package com.platform.kspace.component;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Calendar;
 
+import com.platform.kspace.model.Professor;
+import com.platform.kspace.model.Student;
+import com.platform.kspace.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class DataLoader {
+
+    private UserRepository userRepository;
+
     @Autowired
-    public DataLoader() {
+    public DataLoader(UserRepository userRepository) {
+        this.userRepository = userRepository;
+        LoadData();
+    }
+
+    private void LoadData() {
+        Professor p = new Professor("Petar", "Petrovic", "petar@example.com", "$2a$04$P2R/ohGi2eYUJw02EEZaveX37jwcXb4E.RnwQo8MgP8EgNP0vjfN.");
+        p.setPassword("qwerty", true);
+        p = this.userRepository.save(p);
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(2000, 10, 5);
+        Student s = new Student(
+                "Mika",
+                "Mikic",
+                "mika@example.com",
+                "$2a$04$P2R/ohGi2eYUJw02EEZaveX37jwcXb4E.RnwQo8MgP8EgNP0vjfN.",
+                cal.getTime()
+        );
+        this.userRepository.save(s);
     }
 }
